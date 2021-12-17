502778a31a4b7e4c0fcefd2135da9dc7577554fd
Anthony DeGennaro <amdegennaro@gmail.com>
2021-12-17 18:40:42 -0500

    updates to script for mc sampling

--- scripts/sampler.py
+++ scripts/sampler.py
@@ -1,5 +1,7 @@
 import numpy as np
 import subprocess
+import csv
+import sys , os
 
 
 def compute_eps2_from_X_and_m( X : float , 
@@ -19,22 +21,38 @@ def compute_sigma_from_X_and_m( X : float ,
     return 36 * l_omega**2 / ( ((1-m)/2)**2 * ((1+m)/2)**2 * l_kuhn**2 * X * N**2 )
 
 
-def main( build_dir : str ):
+def main( topdir : str , builddir : str , outdir : str , nsamples : int ):
 
-    m     = 0.5
-    X     = 0.3
+    m     = np.random.uniform( -0.7  , 0.7 , nsamples )
+    X     = np.random.uniform( 0.055 , 0.5 , nsamples )
 
-    eps2  = compute_eps2_from_X_and_m(  X , m )
-    sigma = compute_sigma_from_X_and_m( X , m )
+    for i,(mi,Xi) in enumerate( zip(m,X) ):
 
-    subprocess.run( [ build_dir + "/maDGiCart" , 
-                      "--m"     , str(m) ,
-                      "--eps2"  , str(eps2) ,
-                      "--sigma" , str(sigma) ] )
+        workdir = topdir + '/' + outdir + '/run_' + str(i+1)
+        subprocess.run( [ 'mkdir' , workdir ] )
+        os.chdir( workdir )
+
+        eps2  = compute_eps2_from_X_and_m(  Xi , mi )
+        sigma = compute_sigma_from_X_and_m( Xi , mi )
+
+        subprocess.run( [ topdir + '/' + builddir + "/maDGiCart" ,
+                        "--interactive_plots" , 'false' ,  
+                        "--m"     , str(mi) ,
+                        "--eps2"  , str(eps2) ,
+                        "--sigma" , str(sigma) ] )
+
+        # Save mc parameters
+        w = csv.writer( open( workdir + "/params_" + str(i+1) + ".csv" , "w" ) )
+        w.writerow( [ 'm' , mi ] )
+        w.writerow( [ 'X' , Xi ] )
+
 
 
 if __name__ == "__main__":
 
-    build_dir = '../build'
+    topdir    = sys.argv[1]
+    builddir = sys.argv[2]
+    outdir    = sys.argv[3]
+    nsamples  = int( sys.argv[4] )
 
-    main( build_dir )
+    main( topdir , builddir , outdir , nsamples )
