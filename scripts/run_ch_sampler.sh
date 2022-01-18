8a50bfcc7f9fe0e45a20c4503d2d06f3b4406272
Anthony DeGennaro <amdegennaro@gmail.com>
2022-01-18 18:52:49 -0500

    modify sampler shell script for slurm

--- scripts/run_ch_sampler.sh
+++ scripts/run_ch_sampler.sh
@@ -1,21 +1,22 @@
-#!/bin/bash                                                                                                         
-#                                                                                                                   
-#SBATCH --job-name=mc_ch                                                                                       
-#SBATCH --output=mc_ch.out                                               
-#SBATCH --mail-type=END,FAIL                                                                                        
-#SBATCH --nodes=1                                                                                                   
-#SBATCH --ntasks-per-node=8                                                                                         
-# SBATCH --gres=gpu:1                                                                                               
-# SBATCH -C k40                                                                                                     
-#SBATCH --time=12:00:00                                                                                            
-#SBATCH --mem=32gb                                                                                                  
+#!/bin/bash                                                                                                                            
+#                                                                                                                                      
+#SBATCH --job-name=mc_ch                                                                                                               
+#SBATCH --output=mc_ch.out                                                                                                             
+#SBATCH --mail-type=END,FAIL                                                                                                           
+#SBATCH --nodes=1                                                                                                                      
+#SBATCH --ntasks-per-node=8                                                                                                            
+#SBATCH --gres=gpu:1                                                                                                                   
+#SBATCH -C k40                                                                                                                         
+#SBATCH --time=12:00:00                                                                                                                
+#SBATCH --mem=32gb                                                                                                                     
 
-top_dir=/home/adegennaro/Projects/appmath
+top_dir=/home/adegennaro
 
 nsamples=128
 
 driver_dir=${top_dir}/maDGiCart-CH/scripts
 build_dir=${top_dir}/maDGiCart-CH-build/
 output_dir=${top_dir}/maDGiCart-CH/scripts/output
+sif_dir=${top_dir}/maDGiCart-CH
 
-python3 -u ${driver_dir}/sampler.py ${output_dir} ${build_dir} ${nsamples}
+singularity exec --nv --env LC_ALL=C ${sif_dir}/myimage.sif python3 -u ${driver_dir}/sampler.py ${output_dir} ${build_dir} ${nsamples}
